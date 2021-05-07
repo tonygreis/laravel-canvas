2c770a262e881e616e35ab31ef0335186077b324
tonygreis <tonyxhepa.greis@gmail.com>
2021-05-07 13:25:18 +0200

    free series

--- app/Http/Controllers/Front/WelcomeController.php
+++ app/Http/Controllers/Front/WelcomeController.php
@@ -10,7 +10,7 @@ class WelcomeController extends Controller
 {
     public function welcome()
     {
-        $series = Serie::orderBy('updated_at', 'DESC')->limit(4)->get();
+        $series = Serie::orderBy('updated_at', 'DESC')->limit(6)->get();
         $topics = Topic::with(['published_posts' => function ($query) {
             $query->orderBy('published_at', 'DESC');
         }])->get();
